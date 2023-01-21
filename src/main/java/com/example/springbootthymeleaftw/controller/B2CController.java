a40385b1e069775a9bd2342f1e2305e78ff354af
Radu Alexandru <ralexandru@talentingsoftware.com>
2023-01-21 12:07:58 +0200

    Created form for marfa request.

--- src/main/java/com/example/springbootthymeleaftw/controller/B2CController.java
+++ src/main/java/com/example/springbootthymeleaftw/controller/B2CController.java
@@ -9,12 +9,11 @@ import com.example.springbootthymeleaftw.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 @Controller
@@ -35,20 +34,49 @@ private final UserProductService userProductService;
             }
         }
 
+        System.out.println(this.loggedB2c.getId());
+
+        /// lista produse a b2cului logged
         List<UserProductEntity> listOfB2csWithProducts = userProductService.getAll();
         List<UserProductEntity> result = new ArrayList<UserProductEntity>();
         for (UserProductEntity upe:listOfB2csWithProducts) {
-            for (RoleEntity u:upe.getUser().getRoles()) {
-                if(u.getName().equals(Roles.B2B.toString())){
+            if (upe.getUser().getId()==this.loggedB2c.getId()) {
+                result.add(upe);
+            }
+        }
+
+        model.addAttribute("loggedB2c",this.loggedB2c);
+        model.addAttribute("ProductsOfLoggedB2C",result);
+
+        return "b2c_home";
+    }
+
+    @GetMapping("/RequestMarfa")
+    public String openRequestMarfa(@ModelAttribute("loggedB2c")UserEntity loggedB2c,Model model){
+
+        System.out.println(this.loggedB2c.getId());
+        // lista cu b2b de la care poate b2c sa faca request marfa.
+        List<UserProductEntity> listOfB2bsWithProducts = userProductService.getAll();
+        List<UserProductEntity> result = new ArrayList<UserProductEntity>();
+        for (UserProductEntity upe:listOfB2bsWithProducts) {
+            for (RoleEntity r:upe.getUser().getRoles()) {
+                if(r.getName().equals(Roles.B2B.toString())){
                     result.add(upe);
                     break;
                 }
             }
         }
 
+        model.addAttribute("b2bsWithProducts",result);
         model.addAttribute("loggedB2c",this.loggedB2c);
-        model.addAttribute("b2bs",result);
-        return "b2c";
+        return "b2c_request_marfa";
     }
 
+    @PostMapping("/RequestMarfaPost")
+    public String RequestMarfaPost(@RequestParam Map<String, String> formData,Model model){
+
+        System.out.println("here");
+
+        return "redirect:/B2CController/Open";
+    }
 }
