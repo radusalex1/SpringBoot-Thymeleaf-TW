6b526285165d6e33250fc29df8e4341e8a73507a
Radu Alexandru <ralexandru@talentingsoftware.com>
2023-01-24 17:36:26 +0200

    Filtering done.

--- src/main/java/com/example/springbootthymeleaftw/service/UserProductService.java
+++ src/main/java/com/example/springbootthymeleaftw/service/UserProductService.java
@@ -28,4 +28,38 @@ public class UserProductService {
         return userProductRepository.getByB2bAndProduct(b2cId,productId);
     }
 
+    public List<UserProductEntity> getByFilter(String category,String companyName,Integer quantity)
+    {
+
+        if(!category.equals("All") && !companyName.equals("All") && quantity!=-1) {
+            return userProductRepository.getByFilter(category,companyName,quantity);
+        }
+
+        if(!category.equals("All") && !companyName.equals("All")) {
+            return userProductRepository.getByCategoryAndCompany(category,companyName);
+        }
+
+        if(!category.equals("All") && quantity!=-1){
+            return userProductRepository.getByCategoryAndQuantity(category,quantity);
+        }
+
+        if(!companyName.equals("All") && quantity!=-1){
+            return userProductRepository.getByCompanyAndQuantity(companyName,quantity);
+        }
+
+        if(!category.equals("All") ) {
+            return userProductRepository.getByCategory(category);
+        }
+
+        if(quantity!=-1){
+            return userProductRepository.getByQuantity(quantity);
+        }
+
+        if(!companyName.equals("All")){
+            return userProductRepository.getByCompany(companyName);
+        }
+
+        return userProductRepository.findAll();
+    }
+
 }
